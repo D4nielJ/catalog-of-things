e8007d3c6053b03b71637131da115ac47dabd8aa
d4nielj <d4niel.djm@gmail.com>
2021-10-18 12:57:19 -0500

    Add game class

--- app/classes/item/game.rb
+++ app/classes/item/game.rb
@@ -1,3 +1,15 @@
-class Game 
-  
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer:, last_played_at:, **args)
+    super(**args)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?
+
+  end
 end
