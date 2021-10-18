0eff7c9da6bd7a7ab6a90639188816fb82afbfab
d4nielj <d4niel.djm@gmail.com>
2021-10-18 16:08:30 -0500

    Fix bug and add tests for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,22 @@
-## These test will be implemeted by Daniel
+require_relative '../app/classes/item/game'
+
+describe 'When testing Game class' do
+  it 'should return the correct attr with getters' do
+    test_game = Game.new(date: '2021-10-18', last_played_at: '2021-10-18')
+    expect(test_game.date).to eql '2021-10-18'
+    expect(test_game.last_played_at).to eql '2021-10-18'
+    expect(test_game.multiplayer).to eql false
+  end
+
+  it 'should archive this game' do
+    test_game = Game.new(date: '2010-10-18', last_played_at: '2015-10-18')
+    test_game.move_to_archive
+    expect(test_game.archived).to eql true
+  end
+
+  it 'should not archive this game' do
+    test_game = Game.new(date: '2021-10-18', last_played_at: '2021-10-18')
+    test_game.move_to_archive
+    expect(test_game.archived).to eql false
+  end
+end
