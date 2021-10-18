053182ac6fb58ca28df8e31d7070f5a195158511
d4nielj <d4niel.djm@gmail.com>
2021-10-18 11:01:23 -0500

    Add initialize and setters methods to item

--- app/classes/item/item.rb
+++ app/classes/item/item.rb
@@ -1,4 +1,31 @@
 class Item
-  attr_reader
-  attr_accessor :genre, :author
+  attr_reader :id, :archived
+  attr_accessor :date, :name
+
+  def initialize(name:, archived:, date:, id: Random.rand(1..100_000))
+    @id = id
+    @name = name
+    @date = date
+    @archived = archived
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
 end
