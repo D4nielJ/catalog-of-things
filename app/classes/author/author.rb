886c3b2a52505625ebdb6e747097dc1890b0abc3
d4nielj <d4niel.djm@gmail.com>
2021-10-18 14:20:41 -0500

    Add methods to author class

--- app/classes/author/author.rb
+++ app/classes/author/author.rb
@@ -2,8 +2,15 @@ class Author
   attr_reader :id, :items
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name: 'Anonymous', last_name: '')
+  def initialize(id: Random.rand(1..100_000), first_name: 'Anonymous', last_name: '')
+    @id = id
     @first_name = first_name
     @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.author = self
   end
 end
