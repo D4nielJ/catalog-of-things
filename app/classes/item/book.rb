0ea5cd935259588ef76e57aa64abce9f94f58bce
Alonso Falconi <alonso.fl@live.com>
2021-10-18 15:02:45 -0500

    Add can_be_archived? method in Book class

--- app/classes/item/book.rb
+++ app/classes/item/book.rb
@@ -1,8 +1,15 @@
 class Book < Item
   attr_accessor :
+
   def initialize(publisher:, cover_state:, **args)
     super(**args)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
   end
 end
