39d5dc6e28dceb72d1da2f20d865e0b5c6702ff0
Alonso Falconi <alonso.fl@live.com>
2021-10-18 11:32:51 -0500

    Implement move_to_archive method

--- app/classes/item/item.rb
+++ app/classes/item/item.rb
@@ -29,7 +29,9 @@ class Item
     author.add_item(self) unless && !author.items.include?(self)
   end
 
-  def move_to_archive; end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
   private
 
