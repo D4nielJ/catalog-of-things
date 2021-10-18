c8e2c85c2c7c4721433c93725c3456a3ab12d6e7
Alonso Falconi <alonso.fl@live.com>
2021-10-18 15:22:31 -0500

    Add accessor to Book and Label classes

--- app/classes/item/book.rb
+++ app/classes/item/book.rb
@@ -1,4 +1,6 @@
 class Book < Item
+  attr_accessor :publisher, :cover_state
+
   def initialize(publisher:, cover_state:, **args)
     super(**args)
     @publisher = publisher
--- app/classes/label/label.rb
+++ app/classes/label/label.rb
@@ -1,4 +1,7 @@
 class Label
+  attr_reader :id, :items
+  attr_accessor :title, :color
+
   def initialize(title:, color:, id: Random.rand(1..100_000))
     @id = id
     @title = title
