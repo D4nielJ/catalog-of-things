ccc41b4406a742c7072e79ce90af11ce49d9d22a
Alonso Falconi <alonso.fl@live.com>
2021-10-18 11:44:50 -0500

    Create main.rb file with main method

---
+++ main.rb
@@ -0,0 +1,24 @@
+def main
+  loop do
+    puts
+    puts 'Please choose an option'
+    puts ' 1 - List all books'
+    puts ' 2 - List all music albums'
+    puts ' 3 - List all movies'
+    puts ' 4 - List all games'
+    puts ' 5 - List all genres (e.g \'Comedy\', \'Thriller\')'
+    puts ' 6 - List all labels (e.g. \'Gift\', \'New\')'
+    puts ' 7 - List all authors (e.g. \'Stephen King\')'
+    puts ' 8 - List all sources (e.g. \'From a friend\', \'Online shop\')'
+    puts ' 9 - Add a book'
+    puts '10 - Add a music album'
+    puts '11 - Add a movie'
+    puts '12 - Add a game'
+    puts '13 - Exit'
+    option = gets.chomp
+
+    break if option == '13'
+  end
+end
+
+main
