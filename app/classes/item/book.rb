d8cdc800aebdc7ea1369d55b52b9d0cdd2eb55de
Alonso Falconi <alonso.fl@live.com>
2021-10-18 15:01:09 -0500

    Add Book class with constructor

--- app/classes/item/book.rb
+++ app/classes/item/book.rb
@@ -1,2 +1,8 @@
-class Book
+class Book < Item
+  attr_accessor :
+  def initialize(publisher:, cover_state:, **args)
+    super(**args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 end
