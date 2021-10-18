384df3d3ecb87d7aed7c73758d0f7ce464c0af4b
d4nielj <d4niel.djm@gmail.com>
2021-10-18 11:25:55 -0500

    Revised accesor methods on item

--- app/classes/item/item.rb
+++ app/classes/item/item.rb
@@ -1,5 +1,5 @@
 class Item
-  attr_reader :id, :archived
+  attr_reader :id, :archived, :genre, :source, :label, :author
   attr_accessor :date, :name
 
   def initialize(name:, archived:, date:, id: Random.rand(1..100_000))
@@ -11,21 +11,29 @@ class Item
 
   def genre=(genre)
     @genre = genre
-    genre.items.push(self) unless genre.items.include?(self)
+    genre.add_item(self) unless genre.items.include?(self)
   end
 
   def source=(source)
     @source = source
-    source.items.push(self) unless source.items.include?(self)
+    source.add_item(self) unless !source.items.include?(self)
   end
 
   def label=(label)
     @label = label
-    label.items.push(self) unless label.items.include?(self)
+    label.add_item(self) unless !label.items.include?(self)
   end
 
   def author=(author)
     @author = author
-    author.items.push(self) unless author.items.include?(self)
+    author.add_item(self) unless && !author.items.include?(self)
+  end
+
+  def move_to_archive; end
+
+  private
+
+  def can_be_archived?
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end
 end
