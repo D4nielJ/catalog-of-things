7af820480d2187a29d7a0eaea2c24b8cb6bfec5c
d4nielj <d4niel.djm@gmail.com>
2021-10-19 12:44:31 -0500

    Add actions to create movies and games

---
+++ app/actions/create_game.rb
@@ -0,0 +1,27 @@
+require_relative '../modules/input_module'
+require_relative '../modules/display_module'
+
+class CreateGame
+  include InputModule
+  include DisplayModule
+  attr_reader :name
+
+  def initialize
+    @name = 'Create game'
+  end
+
+  def do_action(state)
+    game = Game.new(**Show_Prompt.call(%i[name date last_played_at multiplayer]))
+    genre = SelectFromList.new.do_helper_action(state, state[:genres], CreateGenre.new, 'Genre')
+    author = SelectFromList.new.do_helper_action(state, state[:authors], CreateAuthor.new, 'Author')
+    label = SelectFromList.new.do_helper_action(state, state[:labels], CreateLabel.new, 'Labels')
+    source = SelectFromList.new.do_helper_action(state, state[:sources], CreateSource.new, 'Source')
+
+    game.label = label
+    game.author = author
+    game.genre = genre
+    game.source = source
+
+    state[:games] << game
+  end
+end
