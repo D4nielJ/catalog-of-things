741c2ca54a46832c7c0d43e73a669720c317c9d7
d4nielj <d4niel.djm@gmail.com>
2021-10-19 18:01:22 -0500

    Error in linters

--- app/classes/file_manager/input_json.rb
+++ app/classes/file_manager/input_json.rb
@@ -1,57 +1,43 @@
 class InputJson
+  MAP_HASH = {
+    authors: lambda { |o, _state, callback:|
+      p 'hello world'
+      Author.new(id: o['id'], first_name: o['first_name'], last_name: o['last_name'])
+    },
+    genres: lambda { |o, _state, callback:|
+      Genre.new(id: o['id'], name: o['name'])
+    },
+    labels: lambda { |o, _state, callback:|
+      Label.new(id: o['id'], title: o['title'], color: o['color'])
+    },
+    sources: lambda { |o, _state, callback:|
+      Source.new(id: o['id'], name: o['name'])
+    },
+    movies: lambda { |o, _state, callback:|
+      new_object = Movie.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
+                             silent: o['silent'])
+      callback(new_object, state)
+    },
+    games: lambda { |o, state, callback:|
+      new_object = Game.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
+                            last_played_at: o['last_played_at'], multiplayer: o['multiplayer'])
+      callback(new_object, state)
+    },
+    albums: lambda { |o, state, callback:|
+      new_object = MusicAlbum.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
+                                  on_spotify: o['on_spotify'])
+      callback(new_object, state)
+    },
+    books: lambda { |o, state, callback:|
+      new_object = Book.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
+                            publisher: o['publisher'], cover_state: o['cover_state'])
+      callback(new_object, state)
+    }
+  }.freeze
+
   def initialize(adress = './app/data')
     @adress = adress
-    @map_hash = {
-      authors: lambda { |o, _state|
-        p 'hello world'
-        Author.new(id: o['id'], first_name: o['first_name'], last_name: o['last_name'])
-      },
-      genres: lambda { |o, _state|
-        Genre.new(id: o['id'], name: o['name'])
-      },
-      labels: lambda { |o, _state|
-        Label.new(id: o['id'], title: o['title'], color: o['color'])
-      },
-      sources: lambda { |o, _state|
-        Source.new(id: o['id'], name: o['name'])
-      },
-      movies: lambda { |o, state|
-        new_object = Movie.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
-                               silent: o['silent'])
-        new_object.author = state[:authors].select { |author| author.id == o['author'] }[0]
-        new_object.source = state[:sources].select { |source| source.id == o['source'] }[0]
-        new_object.genre = state[:genres].select { |genre| genre.id == o['genre'] }[0]
-        new_object.label = state[:labels].select { |label| label.id == o['label'] }[0]
-        new_object
-      },
-      games: lambda { |o, state|
-        new_object = Game.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
-                              last_played_at: o['last_played_at'], multiplayer: o['multiplayer'])
-        new_object.author = state[:authors].select { |author| author.id == o['author'] }[0]
-        new_object.source = state[:sources].select { |source| source.id == o['source'] }[0]
-        new_object.genre = state[:genres].select { |genre| genre.id == o['genre'] }[0]
-        new_object.label = state[:labels].select { |label| label.id == o['label'] }[0]
-        new_object
-      },
-      albums: lambda { |o, state|
-        new_object = MusicAlbum.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
-                               on_spotify: o['on_spotify'])
-        new_object.author = state[:authors].select { |author| author.id == o['author'] }[0]
-        new_object.source = state[:sources].select { |source| source.id == o['source'] }[0]
-        new_object.genre = state[:genres].select { |genre| genre.id == o['genre'] }[0]
-        new_object.label = state[:labels].select { |label| label.id == o['label'] }[0]
-        new_object
-      },
-      books: lambda { |o, state|
-        new_object = Book.new(id: o['id'], name: o['name'], date: o['date'], archived: o['archived'],
-                              publisher: o['publisher'], cover_state: o['cover_state'])
-        new_object.author = state[:authors].select { |author| author.id == o['author'] }[0]
-        new_object.source = state[:sources].select { |source| source.id == o['source'] }[0]
-        new_object.genre = state[:genres].select { |genre| genre.id == o['genre'] }[0]
-        new_object.label = state[:labels].select { |label| label.id == o['label'] }[0]
-        new_object
-      }
-    }
+    @map_hash = MAP_HASH
   end
 
   def fetch_data(state)
@@ -70,12 +56,20 @@ class InputJson
 
   def fetch_file(file, state)
     json = File.read("#{@adress}/#{file}.json")
-    hashes = JSON.parse(json)    
-    arr = hashes.map { |hash| p @map_hash[file.to_sym].call(hash, state) }
+    hashes = JSON.parse(json)
+    arr = hashes.map { |hash| p @map_hash[file.to_sym].call(hash, state, callback: :assign_attr) }
     state[file.to_sym].concat(arr)
   end
 
   def build_authors(hash)
     p "soy un autor #{hash.first_name}"
+  end
+
+  def assign_attr(obj, state)
+    obj.author = state[:authors].select { |author| author.id == o['author'] }[0]
+    obj.source = state[:sources].select { |source| source.id == o['source'] }[0]
+    obj.genre = state[:genres].select { |genre| genre.id == o['genre'] }[0]
+    obj.label = state[:labels].select { |label| label.id == o['label'] }[0]
+    obj
   end
 end
