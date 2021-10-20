e852b6174e3d754a772d41f2810a13cfbdf113d9
Alonso Falconi <alonso.fl@live.com>
2021-10-20 15:45:18 -0500

    Update books table

--- schema.sql
+++ schema.sql
@@ -22,14 +22,14 @@ CREATE TABLE label (
   color VARCHAR(100)
 );
 
-CREATE TABLE book (
+CREATE TABLE books (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  publisher VARCHAR(100),
-  cover_state VARCHAR(100),
-  publish_date DATE,
-  archived BOOLEAN,
   genre_id INT REFERENCES genre(id),
   source_id INT REFERENCES source(id),
   author_id INT REFERENCES author(id),
-  label_id INT REFERENCES label(id)
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100)
 );
