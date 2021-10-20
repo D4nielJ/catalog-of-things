6ae55ed2cd72d72c463771011e4822d72d328662
Alonso Falconi <alonso.fl@live.com>
2021-10-20 15:42:47 -0500

    Create book table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,15 @@ CREATE TABLE label (
   title VARCHAR(100),
   color VARCHAR(100)
 );
+
+CREATE TABLE book (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100),
+  publish_date DATE,
+  archived BOOLEAN,
+  genre_id INT REFERENCES genre(id),
+  source_id INT REFERENCES source(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id)
+);
