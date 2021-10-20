409933150bba01550fc27b1830d730aae84c01f3
Alonso Falconi <alonso.fl@live.com>
2021-10-20 15:48:08 -0500

    Create games table

--- schema.sql
+++ schema.sql
@@ -55,3 +55,15 @@ CREATE TABLE movies (
   archived BOOLEAN,
   silent BOOLEAN
 );
+
+CREATE TABLE games (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT REFERENCES genre(id),
+  source_id INT REFERENCES source(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,
+  last_played_at DATE
+);
